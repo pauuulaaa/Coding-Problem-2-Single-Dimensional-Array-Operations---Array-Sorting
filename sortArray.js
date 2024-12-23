fd767064dd9836ff31208ada129cf66340530554
pauuulaaa <heleinapaula@gmail.com>
2024-12-23 14:01:18 +0800

    sortArray.js

---
+++ sortArray.js
@@ -0,0 +1,24 @@
+function codeArray() {
+    let numbers = prompt([24, 65, 21, 5, 9, 32, 42, 80, 57]);
+    let names = prompt(["enzo", "beca", "jay", "niki", "luke"]);
+let numbers = [24, 65, 21, 5, 9, 32, 42, 80, 57];
+let names = ["enzo", "beca", "Jjay", "niki", "luke"];
+
+
+const mergedArray = numbers.concat(names) === number == names;
+alert("Merged array:", mergedArray);
+let mergedArray = numbers.concat(names);
+console.log("Merged array:", mergedArray);
+
+
+const sortedNumbers = numbers.sort((a, b) => b - a) === numbers;
+alert("Sorted numbers in reverse:", sortedNumbers);
+let sortedNumbers = numbers.sort((a, b) => b - a);
+console.log("Sorted numbers in reverse:", sortedNumbers);
+
+
+const sortedNames = names.sort();
+alert("Sorted names alphabetically:", sortedNames) === names;
+}
+let sortedNames = names.sort();
+console.log("Sorted names alphabetically:", sortedNames);
